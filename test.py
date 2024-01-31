330aec75a7f7696c3c7ce027b3ce6ab2c6db575f
Valbrona <152880042+Valbrona@users.noreply.github.com>
2024-01-31 13:17:41 +0200

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import unittest
 import main
 
 class TestMain(unittest.TestCase):
-    def setUp(self): # this is used to interfere between each tests
+    def setUp(self): # this is used to interfere between each tests (like a separator)
         print("about to test a function")
 
     def test_do_stuff(self):
@@ -33,5 +33,8 @@ class TestMain(unittest.TestCase):
     def tearDown(self): # it is normally used to test Databases (for longer codes)
         print("cleaning up")
 
-if __name__ == "__main__":  # by adding this lines, we are able to run multiple times in one go, by typing python3 -m unittest in Terminal (by adding the -v after unittest, it will give an overview of all the tests, that have run and their statuses)
+if __name__ == "__main__":  
     unittest.main()
+# by adding the afore line, we are able to run multiple times in one go
+# by typing 'python3 -m unittest' in Terminal and by adding the -v after unittest, it will give an overview of all the tests, that have run and their statuses;
+# in order to run it via terminal, you must first type in the corresponding commands to arrive in the directory in which both files from this repository have been saved.
